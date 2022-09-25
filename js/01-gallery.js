da940e58a500e4c059b78d7c067c305393bf3836
Urszula Molska <urszula_molska@wp.pl>
2022-09-25 15:45:44 +0200

    task 1 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></div>`
+  )
+  .join('');
+const toHtml = gallery.insertAdjacentHTML('afterbegin', markup);
 
 console.log(galleryItems);
