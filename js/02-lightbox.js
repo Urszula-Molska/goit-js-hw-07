0b806ecf5411215b56da7cbb7bf11a06434b676e
Urszula Molska <urszula_molska@wp.pl>
2022-09-26 21:41:05 +0200

    Task nr 2-updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,6 +13,25 @@ const toHtml = gallery.insertAdjacentHTML('afterbegin', markup);
 
 console.log(galleryItems);
 
-gallery.addEventListener('click', () => {
+/*gallery.addEventListener('click', event => {
   console.log('image was clicked');
-});
+  event.preventDefault();
+});*/
+gallery.addEventListener('click', handleImage);
+function handleImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let LightboxGallery = new SimpleLightbox('.gallery a');
+  LightboxGallery.on('show.simplelightbox');
+
+  document.addEventListener('keyup', event => {
+    if (event.code === 'Escape') {
+      LightboxGallery.close;
+    }
+  });
+  //});
+  //const LightboxGallery = gallery.simpleLightbox();
+  //LightboxGallery.next();
+}
